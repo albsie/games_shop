e618efa6a39286d3bcb78a692ecd26215f4bb739
XaverL <xlechner@gmx.de>
2021-01-28 10:18:33 +0100

    feat: set Paths right

--- config/db.php
+++ config/db.php
@@ -8,5 +8,5 @@ $password = "";
 
 // $con = mysqli_connect($host, $username, $password, $dbname) or die(mysqli_connect_error());
 
-$con = new PDO('mysql:host=localhost;dbname=games_shop',$username, $password) or die("Verbindung fehlgeschlagen");
+$con = new PDO('mysql:host=localhost;dbname=gamees_shop',$username, $password) or die("Verbindung fehlgeschlagen");
 $con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
