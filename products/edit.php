23d88c546922914249d78e96855acc44cb23527e
leoLackerschmid <leo.lackerschmid@gmx.de>
2021-01-28 12:36:53 +0100

    feat: update Funktion mit Button

--- products/edit.php
+++ products/edit.php
@@ -6,12 +6,27 @@ include_once "../content/header.php";
 $select = "SELECT * FROM products";
 $productItems = $con->query($select) or die(mysqli_error($con));
 ?>
-
+<?php
+# update Befehl für den Button
+if (isset($_POST['register'])) {
+  $SQL_String = "UPDATE products SET price = :price, amount = :amount WHERE id = :id";
+  try {
+    $state = $con->prepare($insert);
+    $state->execute([
+      'price' => $price,
+      'amount' => $amount,
+      'id' => $id
+    ]);
+  } catch(PDOException $e){
+  }
+}
+ ?>
 <main class="container">
   <section>
     <h2>Produkte verwalten</h2>
     <div class="products">
       <form method="post" class="form-inline">
+
         <label for="products">Produkt auswählen</label>
         <select id="products" name="products">
           <?php foreach ($productItems as $key => $value): ?>
@@ -20,6 +35,8 @@ $productItems = $con->query($select) or die(mysqli_error($con));
             </option>
           <?php endforeach ?>
         </select>
+
+
         <button type="submit" name="register" class="btn btn-primary mx-sm-5 mb-3 form-group">Speichern</button>
       </form>
     </div>
