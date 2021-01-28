6b6a44c241213abc176feedc1fab9ca50ca3c761
Jeremiah B <jeremiah@blunser.de>
2021-01-28 12:15:01 +0100

    feat: added dopdown

--- products/edit.php
+++ products/edit.php
@@ -1,9 +1,30 @@
 <?php
+require_once "../config/db.php";
 require_once('../config/config.php');
 include_once "../content/header.php";
+
+$select = "SELECT * FROM products";
+$productItems = $con->query($select) or die(mysqli_error($con));
 ?>
 <main class="container">
-Produkte Verwalten
+<section>
+  <h2>Produkte verwalten</h2>
+
+  <div class="products">
+    <form method="post" class="form-inline">
+      <form method="post" class="form-inline">
+          <label for="products">Produkt auswählen: </label>
+          <select id="products" name="products">
+            <?php foreach ($productItems as $key => $value): ?>
+              <option value="hallo">
+                <?= $value['name'] ?>
+              </option>
+                <?php endforeach ?>
+          </select>
+    <button type="submit" name="register" class="btn btn-primary mx-sm-5 mb-3 form-group">Speichern</button>
+    </form>
+  </div>
+</section>
 </main>
 <?php
 include_once "../content/footer.php";
