08a1993afed12f9674cbb7db58ad4e7279864499
leoLackerschmid <leo.lackerschmid@gmx.de>
2021-02-04 11:44:20 +0100

    feat: init change

--- config/init.php
+++ config/init.php
@@ -1,5 +1,5 @@
 <?php  # root path for the index.php
-define("ROOT_URL", 'games_shop');
+define("ROOT_URL", 'git_games_shop');
 # development root path
-define("ROOT_PATH", "http://localhost/web/games_shop/");
+define("ROOT_PATH", "http://localhost/web/git_games_shop/");
 ?>
