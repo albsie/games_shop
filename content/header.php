a30a4369b0e7b91865301c005c0050a7c73612bc
Sieghard Alber <albsieweb@gmail.com>
2021-02-04 10:02:18 +0100

    feat: new config

--- content/header.php
+++ content/header.php
@@ -1,11 +1,8 @@
 <?php
+require_once("../config/init.php");
 if(session_id() == '' || !isset($_SESSION)) {
     session_start();
 }
-# root path for the index.php
-define("ROOT_URL", 'games_shop');
-# development root path
-define("ROOT_PATH", "http://localhost/IFB/games_shop/");
 
 $path_parts = pathinfo($_SERVER['REQUEST_URI']);
 $filename = $path_parts['filename'];
